103ac32acb574d0d5917436341435a51690f493c
oto1720 <kotaro17206@icloud.com>
2025-12-16 12:00:14 +0900

    feat:app

--- facediary/facediary/app/facediaryApp.swift
+++ facediary/facediary/app/facediaryApp.swift
@@ -10,11 +10,10 @@ import SwiftUI
 @main
 struct facediaryApp: App {
     let persistenceController = PersistenceController.shared
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
